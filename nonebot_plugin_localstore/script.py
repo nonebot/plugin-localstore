337c209a8b9c584828009a8fb178b90d003563bf
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-02-14 03:34:51 +0000

    :sparkles: add dir content

--- nonebot_plugin_localstore/script.py
+++ nonebot_plugin_localstore/script.py
@@ -1,10 +1,11 @@
-import shutil
 from typing import Callable
 from argparse import ArgumentParser
 
+from .utils import remove_dir, show_dir_detail
 from . import BASE_DATA_DIR, BASE_CACHE_DIR, BASE_CONFIG_DIR
 
 parser = ArgumentParser("nb localstore")
+
 
 # main function
 def show_dir():
@@ -24,7 +25,7 @@ cache_subparsers = cache.add_subparsers(title="subcommand")
 
 
 def show_cache():
-    print("Cache Dir: ", BASE_CACHE_DIR)
+    show_dir_detail(BASE_CACHE_DIR)
 
 
 cache.set_defaults(func=show_cache)
@@ -33,10 +34,7 @@ cache_clear = cache_subparsers.add_parser("clear", help="clear cache")
 
 
 def clear_cache():
-    if BASE_CACHE_DIR.exists():
-        confirm = input("Are you sure to clear cache? [y/N] ")
-        if confirm.lower() in {"y", "yes", "1"}:
-            shutil.rmtree(BASE_CACHE_DIR)
+    remove_dir(BASE_CACHE_DIR)
 
 
 cache_clear.set_defaults(func=clear_cache)
@@ -48,7 +46,7 @@ config_subparsers = config.add_subparsers(title="subcommand")
 
 
 def show_config():
-    print("Config Dir: ", BASE_CONFIG_DIR)
+    show_dir_detail(BASE_CONFIG_DIR)
 
 
 config.set_defaults(func=show_config)
@@ -57,10 +55,7 @@ config_clear = config_subparsers.add_parser("clear", help="clear config")
 
 
 def clear_config():
-    if BASE_CONFIG_DIR.exists():
-        confirm = input("Are you sure to clear config? [y/N] ")
-        if confirm.lower() in {"y", "yes", "1"}:
-            shutil.rmtree(BASE_CONFIG_DIR)
+    remove_dir(BASE_CONFIG_DIR)
 
 
 config_clear.set_defaults(func=clear_config)
@@ -72,7 +67,7 @@ data_subparsers = data.add_subparsers(title="subcommand")
 
 
 def show_data():
-    print("Data Dir: ", BASE_DATA_DIR)
+    show_dir_detail(BASE_DATA_DIR)
 
 
 data.set_defaults(func=show_data)
@@ -81,10 +76,7 @@ data_clear = data_subparsers.add_parser("clear", help="clear data")
 
 
 def clear_data():
-    if BASE_DATA_DIR.exists():
-        confirm = input("Are you sure to clear data? [y/N] ")
-        if confirm.lower() in {"y", "yes", "1"}:
-            shutil.rmtree(BASE_DATA_DIR)
+    remove_dir(BASE_DATA_DIR)
 
 
 data_clear.set_defaults(func=clear_data)
