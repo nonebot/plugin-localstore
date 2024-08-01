cac12fa556f1bc1ff220652d69d98fa00a6a1a19
uy_sun <hmy0119@gmail.com>
2024-08-01 16:47:11 +0800

    :white_check_mark: 添加子插件的测试

--- tests/test_get_plugin_dir.py
+++ tests/test_get_plugin_dir.py
@@ -43,3 +43,43 @@ async def test_plugin_dir(app: App, tmp_path: Path):
             True,
         )
         ctx.should_finished(plugin_cmd)
+
+
+async def test_sub_plugin_dir(app: App, tmp_path: Path):
+    """获取到子插件的缓存目录、配置目录和数据目录"""
+    from tests.plugin.plugins.sub_plugin import sub_plugin_cmd
+
+    async with app.test_matcher(sub_plugin_cmd) as ctx:
+        bot = ctx.create_bot()
+        message = make_fake_message()("/sub_plugin cache")
+        event = make_fake_event(_message=message)()
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(
+            event,
+            str(tmp_path / "cache" / "plugin" / "sub_plugin"),
+            True,
+        )
+        ctx.should_finished(sub_plugin_cmd)
+
+        message = make_fake_message()("/sub_plugin config")
+        event = make_fake_event(_message=message)()
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(
+            event,
+            str(tmp_path / "config" / "plugin" / "sub_plugin"),
+            True,
+        )
+        ctx.should_finished(sub_plugin_cmd)
+
+        message = make_fake_message()("/sub_plugin data")
+        event = make_fake_event(_message=message)()
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(
+            event,
+            str(tmp_path / "data" / "plugin" / "sub_plugin"),
+            True,
+        )
+        ctx.should_finished(sub_plugin_cmd)
